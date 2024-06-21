89837cfc9564f5ed99da51fddf128d5a80afbb53
sample436 <j3804174@gmail.com>
2024-06-21 20:19:09 +0530

    Add files via upload

---
+++ alphabet pattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j,n;
+	char ch=65;
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++){
+			cout<<ch;
+			ch++;
+		}
+		cout<<endl;
+	}
+	return 0;
+	
+}
